1e014d9848df7c7884ae4ac4ae8a93070db644c5
jks6404 <jannusingh.75@gmail.com>
2023-08-30 12:01:20 +0530

    submitting code-Wed Aug 30 2023 12:01:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,64 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  function handleSearch(e) {
+    e.preventDefault();
+    // console.log(name)
+    let url = "http://www.omdbapi.com";
+    axios
+      .get(url, {
+        
+        params: {
+          apikey: "a5fe31d8",
+          s: name, //movie name search by user
+          type: "movie",
+        },
+      })
+      .then((response) => {
+        console.log(response.data.Search);
+        setMovies(response.data.Search);
+        setName("");
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h3>Search Movie</h3>
+      <form>
+        <input
+          type="text"
+          placeholder="search"
+          name="search-bar"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit" onClick={handleSearch}>
+          Search
+        </button>
+      </form>
 
-export default App
+      {movies ? (
+        <ul>
+          {movies.map((obj) => {
+            return (
+              <li>
+                <h2>{obj.Title}</h2>
+                <img src={obj.Poster} alt={obj.Title} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <h2 className="error">Invalid movie name. Please try again.</h2>
+      )}
+    </div>
+  );
+};
+
+export default App;
